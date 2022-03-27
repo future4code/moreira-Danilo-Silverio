e690eb453e33e44f18b6684244036de251bd3faa
Danilo Silverio <danilosilverio@outlook.com.br>
2022-03-27 11:41:24 -0300

    Aula Node e Package-json

---
+++ modulo4/node-package-json/exercicio-2/index.js
@@ -0,0 +1,17 @@
+// EXERCÍCIO 2 -------------------------------------------------------------------------------------------->
+
+const firstNumber = Number(process.argv[2]);
+const secondNumber = Number(process.argv[3]);
+
+const result = (a) => {
+    switch(mathOperation){
+        case "add":
+            return firstNumber + secondNumber
+            break;
+        case "subt":
+            //seu código aqui
+            break;
+}
+};
+
+console.log(`Resposta: ${result}`)
---
+++ modulo4/node-package-json/exercicio-3/index.js
@@ -0,0 +1,8 @@
+// EXERCÍCIO 3 -------------------------------------------------------------------------------------------->
+
+let tasks = ["Primeira tafera"];
+
+tasks.push(process.argv[2]);
+
+
+console.log("Tarefas adicionadas com sucesso!", tasks)
