98f170f79e481d1952ffd9df9b144e2a26f7288f
Danilo Silverio <danilosilverio@outlook.com.br>
2022-04-13 09:22:56 -0300

    Revisão Aula Aprofundamento Typescript

---
+++ modulo4/revisao-aprofundamento-typescript/src/exercicio4.ts
@@ -0,0 +1,45 @@
+// EXERCÍCIO 4
+
+/* Na aula de ontem, vimos que os arquivos escritos em Typescript, com sua extensão `.ts` passam por um processo de conversão de typescript para Javascript para que possam ser lidos e executados. Este processo é chamado de **transpilação**. 
+
+Com a biblioteca `typescript` instalada, temos acesso ao comando `tsc` no terminal. O `tsc` é o comando responsável por fazer a transpilação dos arquivos.
+
+Abaixo, há um exemplo de código escrito em Typescript. 
+
+a) Crie um arquivo chamado `exercicio-4.ts` , cole o código abaixo e use comentários para responder as questões.
+
+b) Como você faria, já com a extensão instalada, para transpilar(converter) esse arquivo typescript em um arquivo javascript? */
+//RESPOSTA: Utilizaria o comando tsc (nome do arquivo.extensão) no terminal para transpilar o arquivo.
+//----------------------|
+
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+/* c) E se este arquivo estivesse dentro de uma pasta chamada `src`. O processo seria diferente? Se sim, descreva as diferenças.
+// RESPOSTA: Podemos escrever o caminho do arquivo no terminal para acessa-lo, no caso poderiamos fazer tsc ./src/(nome do arquivo.extensão)
+//----------------------|
+
+d) Existe alguma maneira de **transpilar** múltilplos arquivos de uma vez só? Caso conheça, explique como fazer. */
+//RESPOSTA: O comando tsc sem nenhum parâmetro converte todos os arquivos com extensão ts, ou podemos rodar tsc com o nome de cada arquivo.
+//----------------------|
