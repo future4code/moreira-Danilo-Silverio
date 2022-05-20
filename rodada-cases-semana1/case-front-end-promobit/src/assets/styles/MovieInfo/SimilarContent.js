cda21abdc604fe6d61c1463f75775266f4b57105
Danilo <danilosilverio@outlook.com.br>
2022-05-20 12:07:17 -0300

    Endpoint/Hook/Estruração JSX/Design de filmes recomendados e elenco

--- rodada-cases-semana1/case-front-end-promobit/src/assets/styles/MovieInfo/SimilarContent.js
+++ rodada-cases-semana1/case-front-end-promobit/src/assets/styles/MovieInfo/SimilarContent.js
@@ -1,38 +1,21 @@
 import styled from "styled-components";
 
-export const CastContent = styled.div`
+export const SimilarContent = styled.div`
     display: flex;
     align-items:center;
     flex-direction: column;
     height: fit-content;
     width: 100vw;
 `;
-export const CastTitle = styled.div`
-    display: flex;
-    width: 100vw;
-    h1{
-        position: relative;
-        left: 0;
-        font-size: 1.5rem;
-        padding-left: 5%;
-        margin: 10% 0 1% 0;
-    }
-    @media screen and (max-width: 750px) {
-        h1{
-            font-size: 1rem;
-            margin-bottom: 7%;
-        }
-    }
-`;
-export const CastContainer = styled.div`
+export const SimilarContainer = styled.div`
     display: flex;
     width: 90%;
     padding: 1%;
     gap: 20px;
-    margin-bottom: 10%;
+    margin-bottom: 5%;
     overflow-y: hidden;
     ::-webkit-scrollbar {
-        width: 4px;
+        width: 1px;
         height: 6px;
     }
     ::-webkit-scrollbar-track {
@@ -46,47 +29,58 @@ export const CastContainer = styled.div`
         margin-bottom: 10%;
     }
 `;
-export const CastCard = styled.div`
+export const SimilarTitle = styled.div`
+    display: flex;
+    width: 100vw;
+    h1{
+        position: relative;
+        left: 0;
+        font-size: 1.5rem;
+        padding-left: 5%;
+        margin-bottom: 2%;
+    }
+    @media screen and (max-width: 750px) {
+        h1{
+            font-size: 1rem;
+            margin-bottom: 7%;
+        }
+    }
+`;
+export const RecommendedMovieCard = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
-    width: 13rem;
+    width: 15rem;
     height: 100%;
     cursor: default;
     border-radius: 9px;
-    margin-bottom: 5%;
-    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.25);
-    cursor: default;
+    margin-bottom: 1%;
     img{
-        height: 15rem;
-        width: 13rem;
-        border-top-left-radius: 9px;
-        border-top-right-radius: 9px;
+        height: 10rem;
+        width: 15rem;
+        border-radius: 9px;
+        box-shadow: 4px 4px 14px rgba(0, 0, 0, 0.25);
     }
     h1{
-        font-size: 1rem;
+        font-size: 0.8rem;
         font-weight: 700;
+        padding-top: 3%;
         padding-left: 5%;
     }
     h2{
-        font-size: 0.9rem;
+        font-size: 0.8rem;
         font-weight: 300;
         padding-left: 5%;
     }
     @media screen and (max-width: 750px) {
-        width: 10rem;
+        width: 100%;
+        height: 50vh;
         margin-bottom: 5%;
         img{
-            height: 13rem;
-            width: 10rem;
-        }
-        h1{
-            font-size: 0.8rem;
-        }
-        h2{
-            font-size: 0.7rem;
+            width: 13rem;
+            height: 60%;
+
         }
     }
 `;
-
--- rodada-cases-semana1/case-front-end-promobit/src/pages/MoviesDetails/MovieDetail.js
+++ rodada-cases-semana1/case-front-end-promobit/src/pages/MoviesDetails/MovieDetail.js
@@ -1,78 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import Headers from "../../components/Header/Header";
-import { FeedButton, Details, HeadInfo, InfoCard, MovieGenre, Overview, Poster, MovieInfos } from "../../assets/styles/MovieInfo/MovieInfo"
-import { IMG_API } from "../../constants/url";
-import { getMovieDetail } from "../../services/getMovieById";
+import { FeedButton } from "../../assets/styles/MovieInfo/MovieInfo"
 import FeedLogo from "../../assets/images/feed-logo.png";
-import { getCast } from "../../services/getMovieCast";
-import { CastCard, CastContainer, CastContent, CastTitle } from "../../assets/styles/MovieInfo/CastInfo";
+import { CastContent, CastTitle } from "../../assets/styles/MovieInfo/CastInfo";
+import MovieInformation from "../../components/MovieInformation/MovieInformation";
+import MovieCast from "../../components/MovieInformation/MovieCast";
+import RecommendedMovie from "../../components/MovieInformation/RecommendedMovie";
+import { SimilarContent, SimilarTitle } from "../../assets/styles/MovieInfo/SimilarContent";
 
 const MovieDetail = () => {
 
-  let params = useParams();
   const navigate = useNavigate();
-
-  const [movieInfo, setMovieInfo] = useState([]);
-  const [cast, setCast] = useState([]);
-
-  useEffect(() => {
-    const getApiDetail = async () => {
-      setMovieInfo(await getMovieDetail(params.id));
-      setCast(await getCast(params.id));
-    };
-
-    getApiDetail();
-  }, [params.id]);
-
-  const information = movieInfo.map((item) => {
-    const splitDate = item.release_date.split("-", 3);
-
-    const year = splitDate[0];
-
-    const mounth = splitDate[1];
-
-    const day = splitDate[2];
-
-    const genres = item.genres.map((gen) => {
-      return <p key={gen.id}>{gen.name}</p>;
-    });
-
-    return (
-      <InfoCard key={item.id}>
-        <Poster>
-          <img src={IMG_API + item.poster_path} alt="" />
-        </Poster>
-        <Details>
-          <HeadInfo>
-            <h1>{item.title} ({year})</h1>
-            <MovieGenre>
-              <p>{day}/{mounth}/{year} (BR){" "}</p>
-              {genres}
-            </MovieGenre>
-            <p><span>{item.vote_average}</span> Avaliação dos usuários</p>
-          </HeadInfo>
-          <Overview>
-            <h2>Sinopse</h2>
-            <p>{item.overview}</p>
-          </Overview>
-          <div></div>
-        </Details>
-      </InfoCard>
-    );
-  });
-
-  const castFilm = cast.slice(0, 8).map((c, i) => {
-    return (
-      <CastCard key={i}>
-        <img src={IMG_API + c.profile_path} alt={c.name} />
-        <h1>{c.name}</h1>
-        <h2>{c.character}</h2>
-      </CastCard>
-    )
-  });
-
-
 
   return (
     <div>
@@ -81,19 +20,20 @@ const MovieDetail = () => {
           <img src={FeedLogo} alt="Button to go back to the feed" />
         </FeedButton>
         <main>
-          <MovieInfos>
-            {information}
-          </MovieInfos>
+          <MovieInformation/>
           <CastContent>
             <CastTitle>
               <h1>Elenco original</h1>
             </CastTitle>
-            <CastContainer>
-              {castFilm}
-            </CastContainer>
+            <MovieCast/>
           </CastContent>
           <div></div>
-          <div></div>
+          <SimilarContent>
+            <SimilarTitle>
+              <h1>Recomendações</h1>
+            </SimilarTitle>
+            <RecommendedMovie/>
+          </SimilarContent>
         </main>
     </div>
   );
