198c098e3daed0e06924bf30959d90704bb3e10a
Danilo <danilosilverio@outlook.com.br>
2022-05-23 21:52:00 -0300

    Inicio do Projeto: Set/Cores e Url

---
+++ rodada-cases-semana2/case-frontend-brainnco-sorteio/src/constants/colors.js
@@ -0,0 +1,11 @@
+export const corMegaSena = "#6BEFA3";
+
+export const corQuina = "#8666EF";
+
+export const corLotoFacil = "#DD7AC6";
+
+export const corLotoMania = "#FFAB64";
+
+export const corTimeMania = "#5AAD7D";
+
+export const corDiaDeSorte = "#BFAF83";
